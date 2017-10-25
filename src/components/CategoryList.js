45548b4e93d7815538886ba55c4264e40641aa9c
Steven Lloyd <dwarfmoss@gmail.com>
2017-10-25 06:41:06 -0600

    Sort categories ascending and post descending for number types

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -7,6 +7,21 @@ class CategoryList extends Component {
   render() {
     const { categories } = this.props
     
+    const sortedCategories = categories.sort(function(a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      
+      if (nameA > nameB) {
+        return 1;
+      }
+      
+      // names must be equal
+      return 0;
+    })
+    
     return (
       <div className='categories'>
         <h3>Categories</h3>
@@ -14,7 +29,7 @@ class CategoryList extends Component {
           <li>
             <Category category={{name: 'all', path: ''}} />
           </li>
-          {categories.map(category => (
+          {sortedCategories.map(category => (
             <li key={category.name}>
               <Category category={category} />
             </li>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,14 +4,25 @@ import { connect } from 'react-redux'
 import PostHeader from './PostHeader'
 
 class PostList extends Component {
+  sortPosts = (sortBy) => {
+    switch(sortBy) {
+      case 'author' || 'title' :
+        
+      default :
+        return this.props.posts.sort((a,b) => {
+          return b[sortBy] - a[sortBy]
+        })
+    }
+  }
+  
   render() {
-    const { posts } = this.props
+    const sortedPosts = this.sortPosts('voteScore')
     
     return (
       <div className='posts-view'>
         <h3>Posts</h3>
         <ul className='post-list'>
-          {posts.map(post => (
+          {sortedPosts.map(post => (
             <li key={post.id}>
               <PostHeader post={post} />
             </li>
